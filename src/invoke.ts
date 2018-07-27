a3fca8cd6fb5e31095ab4e07991b1655ed7366ad
TomWan <wanmingtom@gmail.com>
2018-07-27 18:19:55 +0800

    perf: remove useless code

--- src/invoke.ts
+++ src/invoke.ts
@@ -4,13 +4,6 @@ const uuid = require('uuid/v4');
 
 import { sizeof } from './common';
 
-const client = new OSS({
-  region: '<oss region>',
-  accessKeyId: '<Your accessKeyId>',
-  accessKeySecret: '<Your accessKeySecret>',
-  bucket: '<Your bucket name>',
-});
-
 export function initInvoker(options: {
   oss: {
     region: string;
