240bea154fc70cbedca2584e54a1bce9ce01f647
luoyjx <yanjixiong@shimo.im>
2019-11-07 20:48:34 +0800

    fix: test

--- __tests__/storage-spec.ts
+++ __tests__/storage-spec.ts
@@ -3,34 +3,36 @@ import { Client as AWOS } from 'awos-js';
 import { getClientByType } from '../src/storage';
 
 describe('storage', () => {
-  const client = getClientByType('oss', {
-    accessKeyId: '',
-    accessKeySecret: '',
-    bucket: '',
-    endpoint: '',
-  });
-  expect(client instanceof AWOS).toBeTruthy();
-
-  const client1 = getClientByType('aws', {
-    accessKeyId: '',
-    secretAccessKey: '',
-    bucket: '',
-    endpoint: '',
-    s3ForcePathStyle: true,
-    region: '',
-  });
-  expect(client1 instanceof AWOS).toBeTruthy();
-
-  try {
-    getClientByType('abc', {
-      accessKeyId: '',
-      secretAccessKey: '',
-      bucket: '',
-      endpoint: '',
-      s3ForcePathStyle: true,
-      region: '',
+  it('should get storage client by type ok', () => {
+    const client = getClientByType('oss', {
+      accessKeyId: 'your key id',
+      accessKeySecret: 'your secret key',
+      bucket: 'bucket',
+      endpoint: 'oss-cn-beijing.aliyuncs.com',
     });
-  } catch (err) {
-    expect(err).toContain('Unknown store type:');
-  }
+    expect(client instanceof AWOS).toBeTruthy();
+
+    const client1 = getClientByType('aws', {
+      accessKeyId: 'your key id',
+      secretAccessKey: 'your secret key',
+      bucket: 'key',
+      endpoint: 'http://localhost:9000',
+      s3ForcePathStyle: true,
+      region: 'cn-north-1',
+    });
+    expect(client1 instanceof AWOS).toBeTruthy();
+
+    try {
+      getClientByType('abc', {
+        accessKeyId: 'your key id',
+        secretAccessKey: 'your secret key',
+        bucket: 'key',
+        endpoint: 'http://localhost:9000',
+        s3ForcePathStyle: true,
+        region: 'cn-north-1',
+      });
+    } catch (err) {
+      expect(err.message).toContain('Unknown store type:');
+    }
+  })
 });
