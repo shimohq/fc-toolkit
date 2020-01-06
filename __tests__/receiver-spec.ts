962b4053e2367bd51bf2946ea6cfd13dadc43841
liuwenzhe <liuwenzhe_neoye@163.com>
2020-01-06 11:54:57 +0800

    fix: 修复只有当从 oss 获取数据时才需要 JSON.parse

--- __tests__/receiver-spec.ts
+++ __tests__/receiver-spec.ts
@@ -4,12 +4,17 @@ const uuid = require('uuid');
 import * as fs from 'fs';
 import * as path from 'path';
 
-import * as Receiver from '../src/bufferSupport/receiver';
+import * as BufferReceiver from '../src/bufferSupport/receiver';
+import * as Receiver from '../src/receiver';
 import * as storage from '../src/storage';
 import FakeStorage from './mock/storage';
 
 const sandbox = sinon.createSandbox();
 const fakeStorage = new FakeStorage();
+
+const testBody = {
+  deltas: ['aaa', 'bbb'],
+};
 
 describe('receiver test cases', () => {
   beforeAll(() => {
@@ -19,96 +24,148 @@ describe('receiver test cases', () => {
   });
 
   describe('should receive works normally', () => {
-    describe('when storeType is direct', () => {
-      it('when the type of event is string', async () => {
-        const { receive } = Receiver.initReceiver();
-        const resp = await receive(
-          JSON.stringify({
+    describe('buffer support receive', () => {
+      describe('when storeType is direct', () => {
+        it('when the type of event is string', async () => {
+          const { receive } = BufferReceiver.initReceiver();
+          const resp = await receive(
+            JSON.stringify({
+              body: testBody,
+              ossType: 'oss',
+              storeType: 'direct',
+            })
+          );
+          expect(resp.body).toEqual(testBody);
+        });
+
+        it('when the type of event is object', async () => {
+          const { receive } = BufferReceiver.initReceiver();
+          const resp = await receive({
             body: 'test message',
             ossType: 'oss',
             storeType: 'direct',
-          })
-        );
-        expect(resp.body).toBe('test message');
-      });
-
-      it('when the type of event is object', async () => {
-        const { receive } = Receiver.initReceiver();
-        const resp = await receive({
-          body: 'test message',
-          ossType: 'oss',
-          storeType: 'direct',
+          });
+          expect(resp.body).toBe('test message');
         });
-        expect(resp.body).toBe('test message');
-      });
 
-      it('when the type of event is Buffer', async () => {
-        const { receive } = Receiver.initReceiver();
-        const bufferEvent = Buffer.from(
-          JSON.stringify({
-            body: 'test message',
+        it('when the type of event is Buffer', async () => {
+          const { receive } = BufferReceiver.initReceiver();
+          const bufferEvent = Buffer.from(
+            JSON.stringify({
+              body: 'test message',
+              ossType: 'oss',
+              storeType: 'direct',
+            })
+          );
+          const resp = await receive(bufferEvent);
+          expect(resp.body.toString()).toBe('test message');
+        });
+
+        it('should the type of resp.body be Buffer, when event.isBuffer is true', async () => {
+          const { receive } = BufferReceiver.initReceiver();
+          const resp = await receive({
+            body: Buffer.from('test message').toString('base64'),
             ossType: 'oss',
             storeType: 'direct',
-          })
-        );
-        const resp = await receive(bufferEvent);
-        expect(resp.body.toString()).toBe('test message');
+            isBuffer: true,
+          });
+          expect(Buffer.isBuffer(resp.body)).toBeTruthy();
+          expect(resp.body.toString()).toBe('test message');
+        });
       });
 
-      it('should the type of resp.body be Buffer, when event.isBuffer is true', async () => {
-        const { receive } = Receiver.initReceiver();
-        const resp = await receive({
-          body: Buffer.from('test message').toString('base64'),
-          ossType: 'oss',
-          storeType: 'direct',
-          isBuffer: true,
+      describe('when storeType is oss', () => {
+        beforeEach(async () => {
+          this.key = uuid.v4();
+          this.content = 'aaa';
+          await fakeStorage.put(this.key, this.content);
         });
-        expect(Buffer.isBuffer(resp.body)).toBeTruthy();
-        expect(resp.body.toString()).toBe('test message');
+
+        afterEach(async () => {
+          const data = await fakeStorage.getAsBuffer(this.key);
+          expect(data).toBeFalsy();
+        });
+
+        it('should get content from oss works normally', async () => {
+          const { receive } = BufferReceiver.initReceiver();
+          const resp = await receive({
+            body: '',
+            ossType: 'oss',
+            storeType: 'oss',
+            ossKey: this.key,
+          });
+          expect(resp.body).toBe(this.content);
+        });
+
+        it('should the type of resp.body be Buffer, when event.isBuffer is true', async () => {
+          const { receive } = BufferReceiver.initReceiver();
+          const resp = await receive({
+            body: '',
+            ossType: 'oss',
+            storeType: 'oss',
+            ossKey: this.key,
+            isBuffer: true,
+          });
+          expect(Buffer.isBuffer(resp.body)).toBeTruthy();
+          expect(resp.body.toString()).toBe(this.content);
+        });
       });
     });
 
-    describe('when storeType is oss', () => {
-      beforeEach(async () => {
-        this.key = uuid.v4();
-        this.content = 'aaa';
-        await fakeStorage.put(this.key, this.content);
-      });
-
-      afterEach(async () => {
-        const data = await fakeStorage.getAsBuffer(this.key);
-        expect(data).toBeFalsy();
-      });
-
-      it('should get content from oss works normally', async () => {
-        const { receive } = Receiver.initReceiver();
-        const resp = await receive({
-          body: '',
-          ossType: 'oss',
-          storeType: 'oss',
-          ossKey: this.key,
+    describe('receive', () => {
+      describe('when storeType is direct', () => {
+        it('when the type of event is string', async () => {
+          const { receive } = Receiver.initReceiver();
+          const resp = await receive(
+            JSON.stringify({
+              body: testBody,
+              ossType: 'oss',
+              storeType: 'direct',
+            })
+          );
+          expect(resp).toEqual(testBody);
         });
-        expect(resp.body).toBe(this.content);
+
+        it('when the type of event is object', async () => {
+          const { receive } = Receiver.initReceiver();
+          const resp = await receive({
+            body: 'test message',
+            ossType: 'oss',
+            storeType: 'direct',
+          });
+          expect(resp).toBe('test message');
+        });
       });
 
-      it('should the type of resp.body be Buffer, when event.isBuffer is true', async () => {
-        const { receive } = Receiver.initReceiver();
-        const resp = await receive({
-          body: '',
-          ossType: 'oss',
-          storeType: 'oss',
-          ossKey: this.key,
-          isBuffer: true,
+      describe('when storeType is oss', () => {
+        beforeEach(async () => {
+          this.key = uuid.v4();
+          this.content = JSON.stringify(testBody);
+          await fakeStorage.put(this.key, this.content);
         });
-        expect(Buffer.isBuffer(resp.body)).toBeTruthy();
-        expect(resp.body.toString()).toBe(this.content);
+
+        afterEach(async () => {
+          const data = await fakeStorage.getAsBuffer(this.key);
+          expect(data).toBeFalsy();
+        });
+
+        it('should get content from oss works normally', async () => {
+          const { receive } = Receiver.initReceiver();
+          const resp = await receive({
+            body: '',
+            ossType: 'oss',
+            storeType: 'oss',
+            ossKey: this.key,
+          });
+          expect(resp).toEqual(testBody);
+        });
       });
     });
   });
 
   describe('should reply works normally', () => {
     it('when return value is string', done => {
-      const { reply } = Receiver.initReceiver();
+      const { reply } = BufferReceiver.initReceiver();
       const cb = (err, data) => {
         expect(err).toBeFalsy();
         expect(data).toEqual({
@@ -122,7 +179,7 @@ describe('receiver test cases', () => {
     });
 
     it('when return value is Buffer', done => {
-      const { reply } = Receiver.initReceiver();
+      const { reply } = BufferReceiver.initReceiver();
       const cb = (err, data) => {
         expect(err).toBeFalsy();
         expect(data).toEqual({
@@ -136,7 +193,7 @@ describe('receiver test cases', () => {
     });
 
     it('when return value is oss key', done => {
-      const { reply } = Receiver.initReceiver(false, 'oss', 10);
+      const { reply } = BufferReceiver.initReceiver(false, 'oss', 10);
       const cb = (err, data) => {
         expect(err).toBeFalsy();
         expect(data.storeType).toBe('oss');
@@ -149,7 +206,7 @@ describe('receiver test cases', () => {
     });
 
     it('when meta is set', done => {
-      const { reply } = Receiver.initReceiver();
+      const { reply } = BufferReceiver.initReceiver();
       const body = 'foooo';
       const meta = {
         length: body.length,
