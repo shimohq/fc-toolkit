c833c8420b0c6ecc579071cfe7e075d6143c4b77
liuwenzhe <liuwenzhe_neoye@163.com>
2020-03-17 16:54:27 +0800

    [pref] 避免多次创建 oss/aws client

--- src/storage.ts
+++ src/storage.ts
@@ -2,21 +2,35 @@ import { Client as AWOS } from 'awos-js';
 import { IOSSOptions } from 'awos-js/lib/oss';
 import { IAWSOptions } from 'awos-js/lib/aws';
 
+const clientMap: any = {}
+
 export function getClientByType(
   storeType: string,
   options: IOSSOptions | IAWSOptions
 ): AWOS {
   switch (storeType) {
     case 'oss':
-      return new AWOS({
+      const ossClientKey = options.accessKeyId + options.bucket + options.endpoint
+      if (clientMap[ossClientKey]) {
+        return clientMap[ossClientKey]
+      }
+      const ossClient = new AWOS({
         type: 'oss',
         ossOptions: options as IOSSOptions,
       });
+      clientMap[ossClientKey] = ossClient
+      return ossClient
     case 'aws':
-      return new AWOS({
+      const awsClientKey = options.accessKeyId + options.bucket + options.endpoint
+      if (clientMap[awsClientKey]) {
+        return clientMap[awsClientKey]
+      }
+      const awsClient = new AWOS({
         type: 'aws',
         awsOptions: options as IAWSOptions,
       });
+      clientMap[awsClientKey] = awsClient
+      return awsClient
     default:
       throw new Error('Unknown store type: ' + storeType);
   }
