fffc841b05428612988dbeb9ca580fb632c15c0b
liuwenzhe <liuwenzhe_neoye@163.com>
2020-03-17 18:05:08 +0800

    [feat] 支持从 env 读取 oss 配置

--- src/storage.ts
+++ src/storage.ts
@@ -2,7 +2,7 @@ import { Client as AWOS } from 'awos-js';
 import { IOSSOptions } from 'awos-js/lib/oss';
 import { IAWSOptions } from 'awos-js/lib/aws';
 
-const clientMap: any = {}
+const clientMap: any = {};
 
 export function getClientByType(
   storeType: string,
@@ -10,27 +10,29 @@ export function getClientByType(
 ): AWOS {
   switch (storeType) {
     case 'oss':
-      const ossClientKey = options.accessKeyId + options.bucket + options.endpoint
+      const ossClientKey =
+        options.accessKeyId + options.bucket + options.endpoint;
       if (clientMap[ossClientKey]) {
-        return clientMap[ossClientKey]
+        return clientMap[ossClientKey];
       }
       const ossClient = new AWOS({
         type: 'oss',
         ossOptions: options as IOSSOptions,
       });
-      clientMap[ossClientKey] = ossClient
-      return ossClient
+      clientMap[ossClientKey] = ossClient;
+      return ossClient;
     case 'aws':
-      const awsClientKey = options.accessKeyId + options.bucket + options.endpoint
+      const awsClientKey =
+        options.accessKeyId + options.bucket + options.endpoint;
       if (clientMap[awsClientKey]) {
-        return clientMap[awsClientKey]
+        return clientMap[awsClientKey];
       }
       const awsClient = new AWOS({
         type: 'aws',
         awsOptions: options as IAWSOptions,
       });
-      clientMap[awsClientKey] = awsClient
-      return awsClient
+      clientMap[awsClientKey] = awsClient;
+      return awsClient;
     default:
       throw new Error('Unknown store type: ' + storeType);
   }
